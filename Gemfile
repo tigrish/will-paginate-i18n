93854e63a3b234eeaa1c4b702d6d9e28048bd8c1
Christopher Dell <chris@tigrish.com>
2011-12-12 22:15:39 +0100

    Add in i18n-spec

--- Gemfile
+++ Gemfile
@@ -5,4 +5,5 @@ group :development do
   gem "bundler", "~> 1.0.0"
   gem "jeweler", "~> 1.6.4"
   gem "rcov", ">= 0"
+  gem 'i18n-spec'
 end
--- spec/will-paginate-i18n_spec.rb
+++ spec/will-paginate-i18n_spec.rb
@@ -1,7 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "WillPaginateI18n" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+Dir.glob('locale/*.yml').each do |locale_file|
+  describe locale_file do
+    it { should be_parseable}
+    it { should have_valid_pluralization_keys }
   end
 end
