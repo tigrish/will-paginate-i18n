a061d79ec950cebf59dd6770a30bd94d8f5de558
Christopher Dell <chris@tigrish.com>
2011-12-13 01:45:14 +0100

    Bump i18n-spec to 0.0.2

--- spec/will-paginate-i18n_spec.rb
+++ spec/will-paginate-i18n_spec.rb
@@ -1,8 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 Dir.glob('locale/*.yml').each do |locale_file|
-  describe locale_file do
-    it { should be_parseable}
-    it { should have_valid_pluralization_keys }
+  describe 'a locale file' do
+    it_behaves_like 'a valid locale file', locale_file
   end
 end
